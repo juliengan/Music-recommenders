147176793a50c3bcf2235533920458a7452b3966
juliengan <julie.ngan68@gmail.com>
2023-03-06 02:58:35 +0100

    Version allégée

--- web_ml/app.py
+++ web_ml/app.py
@@ -28,30 +28,27 @@ annoy_index = AnnoyIndex(audio_dim, 'angular')
 annoy_index.load('../models/nearest_neightbor_graph.ann')
 
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def recommend(music_input=None):
     """ Use the nearest neighbors of the music liked by the user by using ANNOY. Rule-based chatbot (either recommends music by genre or from a particular artist.)
     """
-    musics = {}
-    for index in range(len(music_dataset[:1000])):
-        sample = music_dataset[index]
-        music_labels = [music_dict[idx] for idx in sample['label']]
-        musics[index] = music_labels
+    if request.method == "GET" or request.method == "POST":
+        musics = {}
+        for index in range(len(music_dataset[:1000])):
+            sample = music_dataset[index]
+            music_labels = [music_dict[idx] for idx in sample['label']]
+            musics[index] = music_labels
 
-    with open("data/musics/ytb_musics_dict", "r") as f:
-        ytb_musics_dict = json.load(f)
-    ytb_df = pd.Series(ytb_musics_dict)
-    music_req_l = request.form.getlist("music_request")
-    music_input = int(music_req_l[0]) if music_req_l else 0 
-    nns_index = annoy_index.get_nns_by_item(music_input, 10)
-    recommends = []
-    for index in nns_index:
-        sample = music_dataset[index]
-        music_labels = [music_dict[idx] for idx in sample['label']]
-        url = "".join(["https://www.youtube.com/watch?v=",sample["video_id"].decode('utf-8')])
-        recommends.append([index, music_labels, url, sample['start_time'], sample['end_time'], ytb_df[index]])
-    playlist = pd.DataFrame(recommends, columns=['index', 'label', 'video_id', 'start_time', 'end_time', 'title'])
-    return render_template("request.html", recommends =recommends, musics = musics, playlist=playlist, music_input=music_input)
+        with open("data/musics/ytb_musics_dict", "r") as f:
+            ytb_musics_dict = json.load(f)
+        ytb_df = pd.Series(ytb_musics_dict)
+        music_req_l = request.form.getlist("music_request")
+        music_input = int(music_req_l[0]) if music_req_l else 0 
+        nns_index = annoy_index.get_nns_by_item(music_input, 10)
+        recommends = []
+        for index in nns_index:
+            recommends.append(ytb_df[index])
+        return render_template("request.html", recommends =recommends, musics = musics)
     
 if __name__ == '__main__':
     app.run(debug=True)
