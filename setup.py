6e5a81988353e2b0b0e820ef385462870a06a0d8
juliengan <julie.ngan68@gmail.com>
2023-03-06 02:14:41 +0100

    Version allégée

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='Chatbot that recommends musics from Spotify Chatbot',
+    description='Chatbot that recommends musics from YoutTube',
     author='Julie NGAN',
     license='',
 )
